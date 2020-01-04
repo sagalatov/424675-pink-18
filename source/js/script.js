24ae978d5db362dcdf49429ed35b42b37bf29abd
sagalatov sergey <sagalatov@mail.ru>
2020-01-04 21:42:24 +0300

    added transparent header for mobile version

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageHeaderLogo = document.querySelector('.page-header__logo');
+var aboutApplicationContainer = document.querySelector('.about-application__container');
+var aboutApplicationSlogan = document.querySelector('.about-application__slogan');
+var aboutApplicationPhoto = document.querySelector('.about-application__photo');
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -7,9 +12,18 @@ navToggle && navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain && navMain.classList.remove('main-nav--closed');
     navMain && navMain.classList.add('main-nav--opened');
+    pageHeaderLogo && pageHeaderLogo.classList.remove('page-header__logo--closed');
+    aboutApplicationContainer && aboutApplicationContainer.classList.remove('about-application__container--closed');
+    aboutApplicationSlogan && aboutApplicationSlogan.classList.remove('about-application__slogan--closed');
+    aboutApplicationPhoto && aboutApplicationPhoto.classList.remove('about-application__photo--closed');
+
   } else {
     navMain && navMain.classList.add('main-nav--closed');
     navMain && navMain.classList.remove('main-nav--opened');
+    pageHeaderLogo && pageHeaderLogo.classList.add('page-header__logo--closed');
+    aboutApplicationContainer && aboutApplicationContainer.classList.add('about-application__container--closed');
+    aboutApplicationSlogan && aboutApplicationSlogan.classList.add('about-application__slogan--closed');
+    aboutApplicationPhoto && aboutApplicationPhoto.classList.add('about-application__photo--closed');
   }
 });
 
