e5d90cc50782867676eae620dd6df588acc8b87c
sagalatov sergey <sagalatov@mail.ru>
2020-01-08 16:48:28 +0300

    add pixelperfect for mobile photo page

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,8 @@ var pageHeaderLogo = document.querySelector('.page-header__logo');
 var aboutApplicationContainer = document.querySelector('.about-application__container');
 var aboutApplicationSlogan = document.querySelector('.about-application__slogan');
 var aboutApplicationPhoto = document.querySelector('.about-application__photo');
+var pageMainPhotos = document.querySelector('.page-main__photos');
+var pageMainTitle = document.querySelector('.page-main__title');
 
 
 navMain.classList.remove('main-nav--nojs');
@@ -16,6 +18,8 @@ navToggle && navToggle.addEventListener('click', function () {
     aboutApplicationContainer && aboutApplicationContainer.classList.remove('about-application__container--closed');
     aboutApplicationSlogan && aboutApplicationSlogan.classList.remove('about-application__slogan--closed');
     aboutApplicationPhoto && aboutApplicationPhoto.classList.remove('about-application__photo--closed');
+    pageMainPhotos && pageMainPhotos.classList.remove('page-main__photos--closed');
+    pageMainTitle && pageMainTitle.classList.remove('page-main__title--closed');
 
   } else {
     navMain && navMain.classList.add('main-nav--closed');
@@ -24,6 +28,8 @@ navToggle && navToggle.addEventListener('click', function () {
     aboutApplicationContainer && aboutApplicationContainer.classList.add('about-application__container--closed');
     aboutApplicationSlogan && aboutApplicationSlogan.classList.add('about-application__slogan--closed');
     aboutApplicationPhoto && aboutApplicationPhoto.classList.add('about-application__photo--closed');
+    pageMainPhotos && pageMainPhotos.classList.add('page-main__photos--closed');
+    pageMainTitle && pageMainTitle.classList.add('page-main__title--closed');
   }
 });
 
